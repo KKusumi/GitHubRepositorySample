6649600d02026320bcd366ac3f94692809a119fd
KKusumi <kusumi_katsumi@andfactory.co.jp>
2021-06-13 20:28:04 +0900

    Initial Commit

---
+++ GitHubRepositorySample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubRepositorySample
+//
+//  Created by kusumi on 2021/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
